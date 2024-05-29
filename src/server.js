d656df8ac6fde87c452b3ecc2881063acc82ecf2
unknown <yyongkisetiawan@gmail.com>
2024-05-29 21:44:19 +0700

    users implemented

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const ClientError = require('./exceptions/ClientError');
+
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -11,6 +18,31 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
---
+++ src/services/postgres/UsersService.js
@@ -0,0 +1,84 @@
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationsError = require('../../exceptions/AuthenticationsError');
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {
+    await this.verifyNewUsername(username);
+
+    const id = `users-${nanoid(16)}`;
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashPassword, fullname],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getUserById(userId) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+    const result = await this._pool.query(query);
+
+    if (result.rows.length > 0) {
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
+    }
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationsError('Krediensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashPassword);
+
+    if (!match) {
+      throw new AuthenticationsError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
+  }
+}
+
+module.exports = UsersService;
